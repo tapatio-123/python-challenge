5681d07daeabcf314876d93aab02ef8f2ae6c0c8
Your Name <sabrina.avila.09@gmail.com>
2020-06-29 22:37:48 -0700

    Homework 3

--- PyBank/main.py
+++ PyBank/main.py
@@ -7,35 +7,44 @@ with open(budget_csv) as csvfile:
     csvreader = csv.reader(csvfile, delimiter= ",")
 
     csv_header = next(csvreader)
-    #print(f"Header: {csv_header}")
 
     total = 0
     months = 0
-    sum_change = 0
-    previous = 0
-
-
-
-    for row in csvreader: 
+    total_months = 0
+    total_change = 0
+    datelist = []
+    pl_list = []
+   
+    for row in csvreader:
+        datelist.append(row[0])
+        pl_list.append(int(row[1]))
         total = total + float (row[1])
         months = months + 1
+    
+    delta = []
+    total_months = len(datelist)
+    for i in range(1,total_months):
+        delta.append(pl_list[i]-pl_list[i-1])
+    
+    inc_pl = max(delta)
+    dec_pl = min(delta)
 
-        change = float (row[1])
-        sum_change = sum_change + change
+    inc_date = datelist[delta.index(max(delta))+1]
+    dec_date = datelist[delta.index(min(delta))+1]
+    
+    average = sum(delta)/(total_months-1)
 
-        average = sum_change / (total - 1)
+average = '${:.2f}'.format(average)
 
-print (f"Total Months: {months}")
+print (f"Total Months: {total_months}")
 print (f"Total: ${total}")
-print (f"Average Change: ${average}")
-#print (f"Greatest Increase in Profits: {}")
-#print (f"Greatest Decrease in Profits: {}")
-
-
+print (f"Average Change: {average}")
+print (f"Greatest Increase in Profits: {inc_date} {inc_pl}")
+print (f"Greatest Decrease in Profits: {dec_date} {dec_pl}")
 
 with open("output.txt", "w") as txt_file:
-    txt_file.write(f"Total months: {months}")
-    txt_file.write(f"Total: {total}")
-    txt_file.write(f"Average Change: {average}")
-    #txt_file.write(f"Greatest Increase in Profits: {}")
-    #txt_file.write(f"Greatest Decrease in Profits: {}")
+    txt_file.write(f"Total months: {months}\n")
+    txt_file.write(f"Total: {total}\n")
+    txt_file.write(f"Average Change: {average}\n")
+    txt_file.write(f"Greatest Increase in Profits: {inc_date} {inc_pl}\n")
+    txt_file.write(f"Greatest Decrease in Profits: {inc_date} {dec_pl}")
--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,34 +1,42 @@
 import os
 import csv
+from collections import Counter
 
 election_csv = os.path.join("..","PyPoll", "Resources", "election_data.csv")
 
 with open(election_csv) as csvfile:
     csvreader = csv.reader(csvfile, delimiter= ",")
-
-
     csv_header = next(csvreader)
 
-votes = 0
-candidates = {}
-
-for row in csvreader: 
-    total = total + float (row[1])
-    votes = votes + 1
+    voters = []
+    candidates_list = []
+    percentage = 0.0
+    percent = []
     
-    if row [2] in candidates:
+    for row in csvreader: 
+        voters.append(row[1])
+        candidates_list.append(row[2])
 
-        pass
-
-    else:
-        pass 
-for key, value in candidates.items ():
-
-    print(f"{key}: + {value}")
-
-    print (f"Total votes: {votes}")
-
+    candidate_votes = Counter(candidates_list)
+    candnames = list(candidate_votes.keys())
+    candvotes = list(candidate_votes.values())
+    popular = max(candvotes)
+    voters = len(candidates_list)
+    print("Election Results")
+    print(f"Total votes: {voters}")
 
 with open("output.txt", "w") as txt_file:
-    txt_file.write(f"Total votes: {votes}")
+    txt_file.write("Election Results\n")
+    txt_file.write(f"Total votes: {voters}\n")
+
+    for i in range (0,len(candvotes)):
+        percentage = (candvotes[i] / float(len(candidates_list)))
+        percent.append(percentage)
+        outputstr = ('{}:'.format(candnames[i]), "{:.2%}".format(percent[i]),'({})'.format(candvotes[i]))
+        outputstr = ' '.join(outputstr)
+        print(outputstr)
+        txt_file.write(outputstr + "\n")
    
+    winner = candvotes.index(popular)
+    print(f"Winner: {candnames[winner]}")
+    txt_file.write(f"Winner: {candnames[winner]}")
